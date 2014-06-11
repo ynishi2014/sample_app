5710d7964007453fb8aeee145803dc77fef75235
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2014-06-11 05:51:23 +0000

    Optimize title of "home"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   let(:base_title){"Ruby on Rails Tutorial Sample App"}
-  describe "Home page" do
 
+  describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -11,17 +11,20 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
-
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("#{base_title} | Help")
@@ -29,12 +32,10 @@ describe "Static pages" do
   end
 
   describe "About page" do
-
     it "should have the content 'About Us'" do 
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("#{base_title} | About Us")
@@ -43,25 +44,14 @@ describe "Static pages" do
 
 
   describe "Contact page" do
-
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
-
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
-
-
-
-
-
-
-
-
-
 
 end
